cd1bbb97abdae84a959e33fe1f5a12f261bfc0b5
YDaewon <qkqh9779@gmail.com>
2025-03-19 16:50:28 +0900

    fix: app.baseUrl 통합

--- backend/finbattle/src/main/java/com/finbattle/domain/oauth/handler/CustomSuccessHandler.java
+++ backend/finbattle/src/main/java/com/finbattle/domain/oauth/handler/CustomSuccessHandler.java
@@ -23,7 +23,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final CookieUtil cookieUtil;
     private final AuthenticationUtil authenticationUtil;
 
-    @Value("${app.clientUrl}")
+    @Value("${app.baseUrl}")
     private String baseUrl;
     private static final String LOGIN_SUCCESS_URI = "/signin?success=true";
 
