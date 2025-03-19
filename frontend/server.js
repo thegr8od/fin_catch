cd0e3e33ed98932d0479ab334da331701faca476
김병년 <qud5252@naver.com>
2025-03-19 14:38:28 +0900

    ci: 사용하지 않는 라이브러리 제거

--- frontend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', () => { /* _ */});
-server.listen(3000);
-
-
-
