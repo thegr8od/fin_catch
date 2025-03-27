1a8affe8fb04e9e7755b8f9259665ddd321177d7
김병년 <qud5252@naver.com>
2025-03-27 11:02:14 +0900

    S12P21D108-206 feat: 오답 문제 조회하기 위한 로직 추가

--- frontend/src/hooks/useAnalyze.ts
+++ frontend/src/hooks/useAnalyze.ts
@@ -11,9 +11,22 @@ interface AnalyzeResponse {
   recommendation: string;
 }
 
+export interface WrongAnswer {
+  quizLogId: number;
+  memberId: number;
+  quizId: number;
+  createdAt: Date;
+  userAnswer: string;
+  isCorrect: boolean;
+}
+
+interface AllWrongAnswer {
+  allWrongAnswer: WrongAnswer[];
+}
+
 export const useAnalyze = () => {
   const { loading, error, execute: analyzeAnswer } = useApi<AnalyzeResponse, AnalyzePayload>("api/ai/analyze", "POST");
-
+  const { execute: readWrongAnswer } = useApi<AllWrongAnswer>("api/quiz/wrong", "GET");
   const analyzeWrongAnswer = useCallback(
     async (quizId: number) => {
       return await analyzeAnswer({
@@ -23,9 +36,14 @@ export const useAnalyze = () => {
     [analyzeAnswer]
   );
 
+  const readAllWrongAnswer = useCallback(async () => {
+    return await readWrongAnswer();
+  }, [readWrongAnswer]);
+
   return {
     loading,
     error,
     analyzeWrongAnswer,
+    readAllWrongAnswer,
   };
 };
