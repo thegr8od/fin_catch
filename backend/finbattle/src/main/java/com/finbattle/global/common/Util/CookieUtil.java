66f6a022361d9cd943aea473957a73402c616ad3
YDaewon <qkqh9779@gmail.com>
2025-04-03 17:08:17 +0900

    refactor: 쿠키,Random 난수 보안 설정

--- backend/finbattle/src/main/java/com/finbattle/global/common/Util/CookieUtil.java
+++ backend/finbattle/src/main/java/com/finbattle/global/common/Util/CookieUtil.java
@@ -9,9 +9,9 @@ public class CookieUtil {
     public Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24 * 60 * 60);
-        //cookie.setSecure(true);  // https 사용시 true
         cookie.setPath("/");
         cookie.setHttpOnly(true);
+        cookie.setSecure(true);  // https 사용시 true
         return cookie;
     }
 }
