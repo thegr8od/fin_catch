47e66e2b458605d949487136f66224db9da88c8a
cosmos2123 <cosmos2123@naver.com>
2025-04-09 09:35:08 +0900

    fix:오답노트 분석

--- frontend/src/components/quiz/WrongQuizList.tsx
+++ frontend/src/components/quiz/WrongQuizList.tsx
@@ -48,6 +48,7 @@ const WrongQuizList: React.FC<WrongQuizListProps> = ({
               {problem.type}
             </span>
           </div>
+          
           <div className="flex justify-between items-center mt-2">
             <p className="text-sm font-korean-pixel" style={{ color: '#EF4444' }}>틀린 횟수: {problem.wrongCount}회</p>
             {/* <p className="text-sm font-korean-pixel" style={{ color: '#34D399' }}>정답 횟수: {problem.correctCount}회</p> */}
